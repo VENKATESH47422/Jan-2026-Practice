d47d5722b75510b30347e27a061fbc068d190437
venkatesh <tommyhilfiger5a7@gmail.com>
2026-01-22 16:16:56 +0530

    practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ID=(id -u)
+ID=$(id -u)
 
 if [ $ID -ne 0]
 then
-    echo " Please login with SUdo user "
+    echo " Error:: Please login with SUdo user "
     exit 1
 else
     echo " Root user login Succesful "
