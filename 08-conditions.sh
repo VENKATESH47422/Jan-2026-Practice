aa14cca23e8d1b3dbf40cad6bc0f616f852d3a84
venkatesh <tommyhilfiger5a7@gmail.com>
2026-01-22 16:02:59 +0530

    practice

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,11 +4,10 @@ Number1=$1
 Number2=$2
 
 if [ $Number1 -gt 100 ]
-then
-    do 
+then 
         Addition=$(($Number1+$Number2))
         echo " 1st is > 100 so Addition:: $Addition ""
-    else 
+else 
         Subtraction=$(($Number1-$Number2))
         echo " 1st is < 100 so Subtraction:: $Subtraction ""
 fi
