9b4f37b7c32a34decc622c781259432904d23884
venkatesh <tommyhilfiger5a7@gmail.com>
2026-01-22 20:55:15 +0530

    practice

---
+++ 10-functions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERRoR:: $2 .. Failed"
+    else
+        echo "$2.. SUCCESS"
+    fi
+}
+
+if [ $ID -ne 0]:
+then
+    echo "Error:: Run this script with root user "
+    exit 1
+else
+    echo "You are a Root user"
+fi
+
+yum install nginx -y
+
+VALIDATE $? "Installing Nginx"
+
+yum install git -y
+
+VALIDATE $? "Installing Git"
+
+yum install telnet -y
+
+VALIDATE $? "Installing Telnet"
