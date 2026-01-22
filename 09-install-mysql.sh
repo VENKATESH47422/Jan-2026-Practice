dbcc3ed3d36aaa2494c389c8ba9d82d94d65bbb0
venkatesh <tommyhilfiger5a7@gmail.com>
2026-01-22 20:40:49 +0530

    practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -12,7 +12,7 @@ fi
 
 yum install mysql -y
 
-if [ $ID -ne 0 ]; 
+if [ $? -ne 0 ]; 
 then
     echo " Error:: MySql installation is Failed "
     exit 1
@@ -23,11 +23,23 @@ fi
 
 yum install nginx -y
 
-if [ $ID -ne 0 ]; 
+if [ $? -ne 0 ]; 
 then
     echo " Error:: Nginx installation is Failed "
     exit 1
 else
     echo " Success:: Nginx installation is Success "
+    exit 1
+fi
+
+
+yum install git -y
+
+if [ $? -ne 0 ]; 
+then
+    echo " Error:: GIt installation is Failed "
+    exit 1
+else
+    echo " Success:: Git installation is Success "
     exit 1
 fi
